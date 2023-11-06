5462023a37ef03dab93ed6e4e6697659be5b397b
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-11-06 20:42:37 +0530

    Update script.js

--- map , filter ,reduce/script.js
+++ map , filter ,reduce/script.js
@@ -1,57 +1,15 @@
 // map , filter and reduce
 // what is map() ?
 
-/*
+/* ANS:
+A Map holds key-value pairs where the keys can be any datatype.
 
+A Map remembers the original insertion order of the keys.
+
+*/
+// what is map() ?
 const nums = [1, 2, 3, 4];
-
 const multiplyThree = nums.map((num, i, arr) => {
-  return num * 3 + i;
-});
-
-console.log(multiplyThree);
-
-*/
-
-/*
-// what is filter()  ?
-
-const nums = [1, 2, 3, 4];
-
-const moreThanTwo = nums.filter((num) => {
-  return num > 2;
-});
-
-console.log(moreThanTwo);
-
-*/
-
-/*
-//what is reduce() ?
-
-const nums = [1, 2, 3, 4];
-
-const sum = nums.reduce((acc, curr, i, arr) => {
-  return acc + curr;
-}, 0);
-console.log(sum);
-*/
-
-// polyfills for map();
-// syntax => Array.map(num,i,arr) =>{});
-
-Array.prototype.myMap = function (cb) {
-  let temp = [];
-  for (let i = 0; i < this.length; i++) {
-    temp.push(cb(this[i], i, this));
-  }
-  return temp;
-};
-
-const nums = [1, 2, 3, 4];
-
-const multiplyThree = nums.myMap((num, i, arr) => {
   return num * 3;
 });
-
 console.log(multiplyThree);
