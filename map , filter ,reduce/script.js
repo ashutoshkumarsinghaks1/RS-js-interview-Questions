9f20a8b28835ad06d89e6510be63225d48dd61ba
ASHUTOSH KUMAR SINGH <72457559+ashutoshkumarsinghaks1@users.noreply.github.com>
2023-11-08 18:05:15 +0530

    Update script.js

--- map , filter ,reduce/script.js
+++ map , filter ,reduce/script.js
@@ -19,4 +19,13 @@ const nums = [1, 2, 3, 4];
 const multiplyThree = nums.map((num, i, arr) => {
   return num * 3;
 });
-console.log(multiplyThree);
+console.log(multiplyThree); //output: [ 3, 6, 9, 12 ]
+
+/*
+const nums = [1, 2, 3, 4];
+const multiplyThree = nums.map((num, i, arr) => {
+  return num * 3 + i;
+});
+ console.log(multiplyThree); //output:[ 3, 7, 11, 15 ]
+
+*/
